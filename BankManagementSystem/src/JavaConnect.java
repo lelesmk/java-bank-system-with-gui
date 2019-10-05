4043d19e3e78de0d17d2d90ca25111783a755267
Lele Mkefa <l.mkefa@gmail.com>
2019-10-05 11:54:25 +0200

    Config connection to database

---
+++ BankManagementSystem/src/JavaConnect.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LelethuMkefa
+ */
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class JavaConnect {
+    
+    Connection conn = null;
+    
+    // Connect to database
+    public static Connection ConnectDb(){
+        
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:/Users/LelethuMkefa/dev/practice-projects/java-practice/BankManagementSystem/bank.db");
+            return conn;
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        return null;
+    }
+}
